12f85ff99e1823e50ccae744a1c92002c245e081
Septiana Yoga <septianayoga30@gmail.com>
2020-12-11 15:49:08 +0700

    move image and welcome into component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import CityCard from './components/CityCard'
 import Navbar from './components/Navbar'
+import ImageAndWelcome from './components/ImageAndWelcome'
 
-import mainImage from "./assets/images/aneka-makanan.png"
 
 const citiesDummy = [
   { id: 74, name: 'Jakarta', country_name: 'Indonesia' },
@@ -13,23 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container-fluid">
-        <div className="row" style={{ marginBottom: 30 }}>
-          <img src={mainImage} width="100%" alt="aneka-makanan"></img>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <div className="card text-white bg-success mb-3 text-center">
-            <div className="card-header"><h1>Welcome to FoodParadise</h1></div>
-            <div className="card-body">
-              <h4 className="card-title">The Easiest Way To Find Restaurants and Food</h4>
-              <p className="card-text">You can grab information about restaurants, cafees, cuisines with just a few clicks.</p>
-              <p className="card-text">Start by choosing the featured cities below, or search the city name.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ImageAndWelcome />
       <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
         <div className="row">
           <div className="col-12">
