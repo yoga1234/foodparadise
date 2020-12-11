46295772efce8598c5af39257af74cf30e5dfc4d
Septiana Yoga <septianayoga30@gmail.com>
2020-12-11 16:38:18 +0700

    adding form for search city

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react'
+
 import Navbar from './components/Navbar'
 import ImageAndWelcome from './components/ImageAndWelcome'
 import FeaturedCities from './components/FeaturedCities'
@@ -8,16 +10,57 @@ const citiesDummy = [
   { id: 170, name: 'Bali', country_name: 'Indonesia' }
 ]
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <ImageAndWelcome />
-      <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
-        <FeaturedCities cities={citiesDummy} />
-      </div>
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      keyword: ''
+    }
+  }
+
+  changeKeywordHandler = (event) => {
+    this.setState({ keyword: event.target.value })
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <ImageAndWelcome />
+        <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
+          <FeaturedCities cities={citiesDummy} />
+          <div className="col">
+            <h3>Search City</h3>
+            <div className="card">
+              <div className="card-body">
+                <div className="form-row">
+                  <div className="col-11">
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder="Type a keyword or city name"
+                      value={this.state.keyword}
+                      onChange={this.changeKeywordHandler}
+                    />
+                  </div>
+                  <div className="col-1">
+                    <button
+                      className="btn btn-primary"
+                      type="button"
+                    > 
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* end of call */}
+        </div>
+        {/* end of container */}
+      </>
+    );
+  }
 }
 
 export default App;
