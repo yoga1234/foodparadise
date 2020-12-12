60d520fc3f35dc92c01ffa41c781c1822f7fde57
Septiana Yoga <septianayoga30@gmail.com>
2020-12-12 17:44:59 +0700

    updating city card using Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home'
+import City from './pages/City'
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Router>
         <Navbar />
-        <Home />
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/city/:city_id" component={City}></Route>
         <Footer />
-      </>
+      </Router>
     );
   }
 }
