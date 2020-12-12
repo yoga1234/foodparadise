1ff2f77eb0c54465407d812f609f5e976b6304f1
Septiana Yoga <septianayoga30@gmail.com>
2020-12-12 17:14:31 +0700

    using env for api key

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const API = {
   zomato: {
     baseUrl: 'https://developers.zomato.com/api/v2.1',
-    api_key: '1673f06286e5d3d7825aa0430db649da'
+    api_key: process.env.REACT_APP_API_KEY_ZOMATO
   }
 }
