784e15ce39acece8ba421c6d3763411dc192d2a1
Septiana Yoga <septianayoga30@gmail.com>
2020-12-12 13:21:43 +0700

    change home into independent file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React, { Component } from 'react'
 
 import Navbar from './components/Navbar'
-import ImageAndWelcome from './components/ImageAndWelcome'
-import CityList from './components/CityList'
-import SearchCity from './components/SearchCity'
 import Footer from './components/Footer/Footer'
-
-const citiesDummy = [
-  { id: 74, name: 'Jakarta', country_name: 'Indonesia' },
-  { id: 11052, name: 'Bandung', country_name: 'Indonesia' },
-  { id: 170, name: 'Bali', country_name: 'Indonesia' }
-]
+import Home from './pages/Home'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      keyword: ''
-    }
-  }
-
-  changeKeywordHandler = (event) => {
-    this.setState({ keyword: event.target.value })
-  }
-
   render() {
     return (
       <>
         <Navbar />
-        <ImageAndWelcome />
-        <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
-          <CityList title={'Featured City'} cities={citiesDummy} />
-          <SearchCity
-            value={this.state.keyword}
-            onChange={this.changeKeywordHandler}
-          />
-          <CityList title={'Search Result'} cities={citiesDummy} />
-        </div>
-        {/* end of container */}
+        <Home />
         <Footer />
       </>
     );
---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+import ImageAndWelcome from '../components/ImageAndWelcome'
+import CityList from '../components/CityList'
+import SearchCity from '../components/SearchCity'
+
+const citiesDummy = [
+  { id: 74, name: 'Jakarta', country_name: 'Indonesia' },
+  { id: 11052, name: 'Bandung', country_name: 'Indonesia' },
+  { id: 170, name: 'Bali', country_name: 'Indonesia' }
+]
+
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      keyword: ''
+    }
+  }
+
+  changeKeywordHandler = (event) => {
+    this.setState({ keyword: event.target.value })
+  }
+
+  render() {
+    return(
+      <>
+        <ImageAndWelcome />
+        <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
+          <CityList title={'Featured City'} cities={citiesDummy} />
+          <SearchCity
+            value={this.state.keyword}
+            onChange={this.changeKeywordHandler}
+          />
+          <CityList title={'Search Result'} cities={citiesDummy} />
+        </div>
+      </>
+    )
+  }
+}
+
+export default Home
