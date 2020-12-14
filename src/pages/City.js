09a48f85c79f3f0f30167b9fd0cd0c18b3796de8
Septiana Yoga <septianayoga30@gmail.com>
2020-12-14 13:17:35 +0700

    updating city page

--- src/pages/City.js
+++ src/pages/City.js
@@ -38,7 +38,8 @@ class City extends Component {
       city: null,
       categories: null,
       categorySelected: null,
-      keyword: ''
+      keyword: '',
+      criteria: []
     }
   }
 
@@ -70,7 +71,13 @@ class City extends Component {
     })
     .then(({ data }) => {
       let city = data.location_suggestions[0]
-      this.setState({ city })
+      let newCriteria = {
+        criteriaName: 'City',
+        data: city
+      }
+      let criteria = [...this.state.criteria]
+      criteria.push(newCriteria)
+      this.setState({ city, criteria })
     })
     .catch(err => console.log(err))
   }
@@ -118,6 +125,39 @@ class City extends Component {
               keyword={this.state.keyword}
               changeKeywordHandler={this.changeKeywordHandler}
             />
+            <div className="card bg-light mb-3" style={{ marginTop: 20 }}>
+              <div className="card-body">
+                <p className="card-title">Find restaurants based on criteria :</p>
+                <table className="table table-hover">
+                  <tbody>
+                    {
+                      this.state.criteria.map((cri, index) => (
+                        <tr key={index} className="table-active">
+                          <td width="40%">{cri.criteriaName} : </td>
+                          <td width="50%">{cri.data.name}</td>
+                          <td>
+                            <i
+                              className="fa fa-times"
+                              aria-hidden="true"
+                              style={{ color: 'red'}}
+                            >
+                            </i>
+                          </td>
+                        </tr>
+                      ))
+                    }
+                  </tbody>
+                </table>
+                <div className="pull-right">
+                  <button
+                    className="btn btn-primary"
+                    type="button"
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
