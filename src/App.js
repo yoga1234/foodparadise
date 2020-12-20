222338cafbfe1c4a72a8829bf95e78c1203c7086
Septiana Yoga <septianayoga30@gmail.com>
2020-12-21 00:04:34 +0700

    adding restaurant detail page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home'
 import City from './pages/City'
+import RestaurantDetail from './pages/RestaurantDetail'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Navbar />
         <Route path="/" exact component={Home}></Route>
         <Route path="/city/:city_id" component={City}></Route>
+        <Route path="/restaurant/:restaurant_id" component={RestaurantDetail}></Route>
         <Footer />
       </Router>
     );
